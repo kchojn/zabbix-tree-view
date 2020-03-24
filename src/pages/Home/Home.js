d20f3fa79d7400678c813cce6edf5a3792074a88
Karol Chojnowski <karol.chojnowski@hawatel.com>
2020-03-24 14:19:19 +0100

    refacctor

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import  React, {Component} from 'react';
 import {withStyles} from "@material-ui/core/styles";
 import styles from "./styles"
-import Settings from "../../components/ZabbixSettings/ZabbixSettings";
+import Settings from "../../components/Authentication/AuthenticationSettings";
 
 class Home extends Component {
     render() {
